82a4563894c434e40d337dea7a8f9c740f567593
Daniil Oliynyk <daniil.oliynyk@yahoo.ca>
2025-03-29 15:21:03 -0400

    Finish base for parser. Can parse bulk array of strings. Nested arrays not supported yet. Do ECHO next

--- tcp_server.go
+++ tcp_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"strconv"
 )
@@ -61,41 +62,7 @@ func handleConn(conn net.Conn, errCh chan<- error) {
 
 	// buff := make([]byte, 128)
 	for {
-
-		var bt byte
-
-		bt, err := client.rd.ReadByte()
-		if err != nil {
-			fmt.Println("ReadByte Error: ", err)
-			errCh <- err
-		}
-
-		if bt == '*' {
-			// Now we know we have a multibulk array
-			// Next step is to get the length and proceed with reading it
-			btint, err := client.rd.ReadBytes('\n')
-			if err != nil {
-				fmt.Println("ReadBytes Error: ", err)
-				errCh <- err
-			}
-			// ReadBytes will also read \r\n so [:len(btint)-2] will remove the
-			// last 2 bytes which wil be \r\n
-			bulkArrLen, err := strconv.ParseInt(string(btint[:len(btint)-2]), 10, 64)
-			if err != nil {
-				fmt.Println("ParseInt Error: ", err)
-				errCh <- err
-			}
-			fmt.Println(bulkArrLen)
-
-			for i := 0; i < int(bulkArrLen); i++ {
-				// Now we need to read each element in the bulk array
-				// There are bulkArrLen amount of elements
-
-			}
-
-		}
-
-		// nread, err := conn.Read(buff)
+		// nread, err := client.con.Read(buff)
 		// if err != nil {
 		// 	if err == io.EOF {
 		// 		errCh <- nil
@@ -109,7 +76,12 @@ func handleConn(conn net.Conn, errCh chan<- error) {
 		// s := fmt.Sprintf("command:\n%s", buff[:nread])
 		// fmt.Println(s)
 
-		_, err = conn.Write([]byte("+PONG\r\n"))
+		st := readVal(client.rd)
+
+		fmt.Println("handleConn.readVal st=", st)
+		fmt.Println("handleConn.readVal st[1]=", st[0])
+
+		_, err := conn.Write([]byte("+PONG\r\n"))
 		if err != nil {
 			fmt.Println("Write Response Error:", err)
 			errCh <- err
@@ -117,4 +89,92 @@ func handleConn(conn net.Conn, errCh chan<- error) {
 			// return err
 		}
 	}
+}
+
+func readVal(rd *bufio.Reader) []interface{} {
+
+	var bt byte
+
+	vals := make([]interface{}, 0)
+
+	bt, err := rd.ReadByte()
+	if err != nil {
+		fmt.Println("readVal.ReadByte Error: ", err)
+
+	}
+
+	if bt == '*' {
+		// Now we know we have a multibulk array
+		// Next step is to get the length and proceed with reading it
+		vals = append(vals, readArrayVal(rd))
+
+	} else {
+
+		// Simple strings 	RESP2 	Simple 	+
+		if bt == '+' {
+
+		}
+		// Integers 	RESP2 	Simple 	:
+		if bt == ':' {
+
+		}
+		// Bulk strings 	RESP2 	Aggregate 	$
+		if bt == '$' {
+			btint, err := rd.ReadBytes('\n')
+			if err != nil {
+				fmt.Println("readVal.$.ReadBytes Error: ", err)
+
+			}
+			bulkValLen, err := strconv.ParseInt(string(btint[:len(btint)-2]), 10, 64)
+			if err != nil {
+				fmt.Println("readVal.$.ParseInt Error: ", err)
+
+			}
+			fmt.Println("readVal bulkValLen=", bulkValLen)
+
+			// +2 because \n\r will also be read
+			bulkStringBuff := make([]byte, bulkValLen+2)
+			nread, err := io.ReadFull(rd, bulkStringBuff)
+			if err != nil {
+				fmt.Println("readVal.$.ReadFull Error: ", err)
+			}
+			fmt.Println("readVal.$.ReadFull nread=", nread)
+			s := fmt.Sprintf("command:\n%s", bulkStringBuff)
+			fmt.Println(s)
+			vals = append(vals, string(bulkStringBuff))
+
+		}
+	}
+	return vals
+}
+
+func readArrayVal(rd *bufio.Reader) []interface{} {
+	btint, err := rd.ReadBytes('\n')
+	if err != nil {
+		fmt.Println("readArrayVal.ReadBytes Error: ", err)
+
+	}
+	// ReadBytes will also read \r\n so [:len(btint)-2] will remove the
+	// last 2 bytes which wil be \r\n
+	bulkArrLen, err := strconv.ParseInt(string(btint[:len(btint)-2]), 10, 64)
+	if err != nil {
+		fmt.Println("readArrayVal.ParseInt Error: ", err)
+
+	}
+	fmt.Println("readArrayVal bulkArrLen=", bulkArrLen)
+
+	// In the case of nested arrays, bulkArrLen is doesnt count number of items
+	// in the nested array. So i thinkk the array will be atleast bulkArrLen and will
+	// have to dynamically resize itself
+	// Actually atm I dont think nested arrays will even parse
+	bulkArrayVals := make([]interface{}, bulkArrLen)
+
+	for i := 0; i < int(bulkArrLen); i++ {
+		// Now we need to read each element in the bulk array
+		// There are bulkArrLen amount of elements
+
+		val := readVal(rd)
+		bulkArrayVals[i] = val
+	}
+	return bulkArrayVals
 }
